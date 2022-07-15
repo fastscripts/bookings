bc0e44329e2069459f4c08d4932b3e7fc7bed4d3
Michael Kunkel <micha@localhost>
2022-07-15 21:07:50 +0200

    Lektion 71 error response to user

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/fastscripts/bookings/internal/config"
 	"github.com/fastscripts/bookings/internal/handlers"
+	"github.com/fastscripts/bookings/internal/models"
 	"github.com/fastscripts/bookings/internal/render"
 )
 
@@ -19,6 +21,11 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+
+	// what am I going to put in the session
+
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 
